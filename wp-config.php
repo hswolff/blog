81511f5ab46445df80a73aaef392d2ef3d6405f6
Harry Wolff <hswolff@gmail.com>
2012-03-06 23:42:46 -0500

    misc updates

---
+++ wp-config.php
@@ -0,0 +1,5 @@
+<?php
+// Settings used while devving
+define('WP_DEBUG', true);
+define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/harrywolff.com/wp-content');
+define('WP_CONTENT_URL', 'http://local.hswolff.com/harrywolff.com/wp-content');

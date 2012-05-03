100e0d8cb3089cf1cccfdfa9fbd9142e2d240fd8
Harry Wolff <hswolff@gmail.com>
2012-05-02 23:12:27 -0400

    these are whoopsie mistakes

--- wp-content/themes/harrywolff/header.php
+++ wp-content/themes/harrywolff/header.php
@@ -104,7 +104,7 @@
 		<div id="branding-hello" class="clearfix hentry">
 			<div class="content">
 				<h1>Hello.  My name is <strong>Harry Wolff</strong>.</h1>
-				<p>I am web developer working at <a href="http://getglue.com/">GetGlue</a>.</p>
+				<p>I am a web developer working at <a href="http://getglue.com/">GetGlue</a>.</p>
 				<p>I enjoy <a href="http://twitter.com/hswolff">tweeting</a>, <a href="http://ink361.com/hswolff">taking pictures</a>, and <a href="http://npeaches.org/">making movies</a>.</p>
 				<p>This is where I write.</p>
 			</div>

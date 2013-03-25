4b90bd467c0818d81247c2cefbcf48f212ad8692
Harry Wolff <hswolff@gmail.com>
2013-03-24 23:39:40 -0400

    fix heading nav when using menu

--- wp-content/themes/harrywolff_2/library/bones.php
+++ wp-content/themes/harrywolff_2/library/bones.php
@@ -235,10 +235,8 @@ MENUS & NAVIGATION
 function bones_main_nav() {
 	// display the wp3 menu if available
     wp_nav_menu(array(
-    	'container' => false,                           // remove nav container
-    	'container_class' => 'menu clearfix',           // class of container (should you choose to use it)
+    	'container_class' => 'nav top-nav clearfix',
     	'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-    	'menu_class' => 'nav top-nav clearfix',         // adding custom nav class
     	'theme_location' => 'main-nav',                 // where it's located in the theme
     	'before' => '',                                 // before the menu
         'after' => '',                                  // after the menu

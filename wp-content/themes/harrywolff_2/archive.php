2b624a476146e1f048f1f0954190c865f9efb03f
Harry Wolff <hswolff@gmail.com>
2013-07-07 15:03:00 -0400

    generalize the_post format to be used across multiple templates

--- wp-content/themes/harrywolff_2/archive.php
+++ wp-content/themes/harrywolff_2/archive.php
@@ -8,7 +8,7 @@
 
 							<?php if (is_category()) { ?>
 								<h1 class="archive-title h2">
-									<span><?php _e("Posts Categorized:", "bonestheme"); ?></span> <?php single_cat_title(); ?>
+									<?php single_cat_title(); ?> <span><?php _e("Posts", "bonestheme"); ?></span>
 								</h1>
 
 							<?php } elseif (is_tag()) { ?>
@@ -43,30 +43,7 @@
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-
-								<header class="article-header">
-
-									<h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-									<p class="byline vcard"><?php
-										printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(__('F jS, Y', 'bonestheme')), bones_get_the_author_posts_link(), get_the_category_list(', '));
-									?></p>
-
-								</header> <!-- end article header -->
-
-								<section class="entry-content clearfix">
-
-									<?php the_post_thumbnail( 'bones-thumb-300' ); ?>
-
-									<?php the_excerpt(); ?>
-
-								</section> <!-- end article section -->
-
-								<footer class="article-footer">
-
-								</footer> <!-- end article footer -->
-
-							</article> <!-- end article -->
+								<?php get_template_part('the_post'); ?>
 
 							<?php endwhile; ?>
 

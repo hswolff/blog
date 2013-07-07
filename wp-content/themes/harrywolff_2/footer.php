e88cc1bca212f7fac6601fa32fc16d77c99de728
Harry Wolff <hswolff@gmail.com>
2013-07-07 14:40:17 -0400

    update footer, make it a little more friendly and personable

--- wp-content/themes/harrywolff_2/footer.php
+++ wp-content/themes/harrywolff_2/footer.php
@@ -6,7 +6,14 @@
 							<?php bones_footer_links(); ?>
 									</nav>
 
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+					<p class="source-org copyright">Written by <strong>Harry</strong>.</p>
+
+					<blockquote class="footer-poem">
+						And as he typed,
+						his fingers clacked,
+						Stroking the keys
+						to share his ideas.
+					</blockquote>
 
 				</div> <!-- end #inner-footer -->
 

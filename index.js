cf5732d66eccb426ed46957133d411e4d2bcc7f6
Harry Wolff <hswolff@gmail.com>
2014-01-05 20:45:29 -0500

    add basic files for blog

---
+++ index.js
@@ -0,0 +1,12 @@
+// It's spooky time, Ghost time.
+
+var ghost = require('ghost'),
+    path  = require('path');
+
+// Once we can pass config file in as
+// start up option we'll use htis.
+// ghost({
+//   config: path.join(__dirname, 'config.js')
+// });
+
+ghost();

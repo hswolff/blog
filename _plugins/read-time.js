32f2ee4d9b9ff76ffb3d95a7d99e6500dc77b5d6
Harry Wolff <hswolff@gmail.com>
2016-02-04 08:46:49 -0500

    fix read time for 0.8.0 compatibility

--- _plugins/read-time.js
+++ _plugins/read-time.js
@@ -1,9 +1,9 @@
 var readTime = require('read-time');
 
 module.exports = function(Plugin) {
-  Plugin.event.file.beforeRender(function(file) {
+  Plugin.event.file.afterRender(function(file) {
     if (file.collectionIds.has('post')) {
-      file.data.readTime = readTime(file.content).text;
+      file.data.readTime = readTime(file.data.content).text;
     }
   });
 };

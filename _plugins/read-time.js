62880a58ecf4c5c98acf3c5fd460539e63c13f93
Harry Wolff <hswolff@gmail.com>
2016-02-05 21:20:34 -0500

    fix read time plugin for yarn 0.8.0 compat

--- _plugins/read-time.js
+++ _plugins/read-time.js
@@ -1,7 +1,7 @@
 var readTime = require('read-time');
 
 module.exports = function(Plugin) {
-  Plugin.event.file.afterRender(function(file) {
+  Plugin.event.file.afterRender(function(file, renderedFile) {
     if (file.collectionIds.has('post')) {
       file.data.readTime = readTime(file.data.content).text;
     }
